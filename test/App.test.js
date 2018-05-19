83f4419cf8823d8390f1e2cbbbae529bbfa5d2d3
c-lack <c.lack@warwick.ac.uk>
2018-05-19 18:21:27 +0100

    Implemented tests

---
+++ test/App.test.js
@@ -0,0 +1,12 @@
+import App from '../src/components/App.js';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+describe('App', () => {
+  test('should match snapshot', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find('h1').text()).toBe('Welcome to React');
+    expect(wrapper).toMatchSnapshot;
+  })
+})
