90b3596f45bd4bfa1d32c9fe00a3bc874a05a0a1
c-lack <c.lack@warwick.ac.uk>
2018-05-19 19:03:21 +0100

    Implemented webpack

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 
-ReactDom.render(
+import './styles/style.sass';
+
+ReactDOM.render(
   <App />,
   document.getElementById('app')
 );
